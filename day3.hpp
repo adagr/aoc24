b5d16fc8342addc5d10a9b006f0bc0bd95d6c5b0
Adam Green <xxxxx_120@hotmail.com>
2024-12-03 22:41:25 +0100

    day 3

---
+++ day3.hpp
@@ -0,0 +1,41 @@
+#include "day.hpp"
+#include <regex>
+#include <sstream>
+
+class Day3 : public Day {
+ public:
+  Day3() : Day{"input/day3.txt"} {}
+  ~Day3() override = default;
+
+  void run() override {
+    std::regex regexMult("mul\\(\\d+,\\d+\\)");
+    std::regex regexDo("do\\(\\)");
+    std::regex regexDont("don\\'t\\(\\)");
+    std::smatch smDo, smDont, smMult;
+    bool doMult{true};
+    int sum{};
+    for (auto line : lines) {
+      bool res{true};
+      while (res) {
+        std::regex_search(line, smDo, regexDo);
+        std::regex_search(line, smDont, regexDont);
+        res = std::regex_search(line, smMult, regexMult);
+        if ((smDo.position() < smDont.position() || smDont.empty()) && smDo.position() < smMult.position()) {
+          doMult = true;
+        } else if (smDont.position() < smMult.position()) {
+          doMult = false;
+        }
+
+        if (res && doMult) {
+          auto pos = smMult.str().find(",");
+          sum += std::stoi(smMult.str().substr(4, smMult.str().size())) * stoi(smMult.str().substr(pos + 1, smMult.str().size()));
+        }
+
+        line = smMult.suffix();
+      }
+    }
+
+    std::cout << sum << std::endl;
+  }
+
+};
